f2bf12c2551cc0e38ba52ddeaae7b65153c939a0
Idoia <ipalomabel1@educacion.navarra.es>
2020-10-24 12:49:54 +0200

    Parametrizada la clase y modificado metodo Accion

--- Vendedor.cs
+++ Vendedor.cs
@@ -7,10 +7,14 @@ namespace fabricantevendedor
     {
         private Almacen _a;
         private Thread _t;
-        private Random _rnd = new Random();
-        public Vendedor(Almacen a)
+        //private Random _rnd = new Random();
+        private int ms_vender;
+        private int cantidad;
+        public Vendedor(Almacen a, int ms_vender, int cantidad)
         {
             this._a = a;
+            this.ms_vender = ms_vender
+            this.cantidad = cantidad
         }
 
         public void Vende()
@@ -26,13 +30,18 @@ namespace fabricantevendedor
 
         private void _Accion()
         {
-            int ms;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < cantidad; i++)
             {
-                ms = _rnd.Next(1000, 1500);
-                Thread.Sleep(ms);
+                Thread.Sleep(ms_vender);
                 _a.Sacar();
             }
+            // int ms;
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     ms = _rnd.Next(1000, 1500);
+            //     Thread.Sleep(ms);
+            //     _a.Sacar();
+            // }
         }
     }
 }
